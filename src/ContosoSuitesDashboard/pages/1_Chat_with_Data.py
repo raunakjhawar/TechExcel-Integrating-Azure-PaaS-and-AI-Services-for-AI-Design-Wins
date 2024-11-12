b8dd05fc1afe8c05012d3a13304945924203400d
Raunak Jhawar <110283752+raunakjhawar@users.noreply.github.com>
2024-11-12 16:09:40 +0000

    Missing code block updated

--- src/ContosoSuitesDashboard/pages/1_Chat_with_Data.py
+++ src/ContosoSuitesDashboard/pages/1_Chat_with_Data.py
@@ -11,9 +11,14 @@ def create_chat_completion(messages):
     # This is a secure way to store sensitive information that you don't want to expose in your code.
     # Learn more about Streamlit secrets here: https://docs.streamlit.io/develop/concepts/connections/secrets-management
     # The secrets themselves are stored in the .streamlit/secrets.toml file.
-    aoai_endpoint = st.secrets["search"]["endpoint"]
-    aoai_key = st.secrets["search"]["key"]
-    aoai_deployment_name = st.secrets["search"]["index_name"]
+    aoai_endpoint = st.secrets["aoai"]["endpoint"]
+    aoai_key = st.secrets["aoai"]["key"]
+    aoai_deployment_name = st.secrets["aoai"]["deployment_name"]
+
+    search_endpoint = st.secrets["search"]["endpoint"]
+    search_key = st.secrets["search"]["key"]
+    search_index_name = st.secrets["search"]["index_name"]
+
 
     client = openai.AzureOpenAI(
         api_key=aoai_key,
